fcd7d5c5d61b0643d6629342ee7d0b7f2eeebc9f
Binit Dhakal <binit.dhakal101@protonmail.com>
2024-01-30 08:18:17 +0545

    feat: added which-key and yanky

--- lua/binit/plugins/telescope.lua
+++ lua/binit/plugins/telescope.lua
@@ -1,81 +1,103 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    branch='0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build='make',
-        cond=function()
-          return vim.fn.executable 'make' == 1
-        end
-      }
-    },
-    config=function()
-        local keymap = vim.keymap
-        local telescope = require("telescope")
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end
+    }
+  },
+  config = function()
+    local keymap = vim.keymap
+    local telescope = require("telescope")
 
-        telescope.setup {
-          defaults = {
-            vimgrep_arguments = {
-              "rg",
-              "-L",
-              "--color=never",
-              "--no-heading",
-              "--with-filename",
-              "--line-number",
-              "--column",
-              "--smart-case",
-            },
-            prompt_prefix = "   ",
-            selection_caret = "  ",
-            entry_prefix = "  ",
-            initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "ascending",
-            layout_strategy = "horizontal",
-            layout_config = {
-              horizontal = {
-                prompt_position = "top",
-                preview_width = 0.55,
-                results_width = 0.8,
-              },
-              vertical = {
-                mirror = false,
-              },
-              width = 0.87,
-              height = 0.80,
-              preview_cutoff = 120,
-            },
-            file_sorter = require("telescope.sorters").get_fuzzy_file,
-            file_ignore_patterns = { "node_modules" , "venv"},
-            generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-            path_display = { "truncate" },
-            winblend = 0,
-            border = {},
-            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-            color_devicons = true,
-            set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-            -- Developer configurations: Not meant for general override
-            buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-            mappings = {
-              n = { ["q"] = require("telescope.actions").close },
-            },
+    telescope.setup {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "-L",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
           },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules", "venv" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+        mappings = {
+          n = { ["q"] = require("telescope.actions").close },
+        },
+      },
 
-          extensions_list = { "themes", "terms" },
-          
-        } 
+      extensions_list = { "themes", "terms" },
 
-        --- key mapping ---
-        local builtin = require("telescope.builtin")
-        keymap.set('n', '<leader>ff', builtin.find_files, {})
-        keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        keymap.set('n', '<leader>fb', builtin.buffers, {})
-        keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    }
+
+    --- key mapping ---
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to telescope to change theme, layout, etc.
+      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+
+    local function telescope_live_grep_open_files()
+      require('telescope.builtin').live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+      }
     end
-  }
-
+    vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch Buffer" })
+  end
+}
